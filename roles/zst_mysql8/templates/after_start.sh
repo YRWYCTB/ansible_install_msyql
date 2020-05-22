8347d5380528154e7402b832affe665f75764772
YRWYCTB <39433652+YRWYCTB@users.noreply.github.com>
2020-05-22 14:00:31 +0800

    Update after_start.sh
    change the passwd of root@localhost after starting mysql.

--- roles/zst_mysql8/templates/after_start.sh
+++ roles/zst_mysql8/templates/after_start.sh
@@ -4,3 +4,5 @@ echo "DATA_DIR {{ DATA_DIR }}"
 
 echo "指定你需要的初始化相关内容"
 
+#change root password
+{{ BASE_DIR }}/bin/mysql -S /tmp/mysql{{ MYSQL_PORT }}.sock -e "alter user user() identified by 'passwd'"
