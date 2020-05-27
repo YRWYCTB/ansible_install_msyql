f9d82f65ef35b386ff3c815009500c37f2d449b3
YRWYCTB <281153738@qq.com>
2020-05-27 03:25:18 +0000

    modified mysql8 cnf

--- roles/zst_mysql8/templates/after_start.sh
+++ roles/zst_mysql8/templates/after_start.sh
@@ -3,6 +3,8 @@ echo "安装的mysql版本 {{ MYSQL_VER }}"
 echo "DATA_DIR {{ DATA_DIR }}"
 
 echo "指定你需要的初始化相关内容"
-
 #change root password
-{{ BASE_DIR }}/bin/mysql -S /tmp/mysql{{ MYSQL_PORT }}.sock -e "alter user user() identified by 'passwd'"
+{{ BASE_DIR }}/bin/mysql -S /tmp/mysql{{ MYSQL_PORT }}.sock -e "alter user user() identified by '8085782'"
+{{ BASE_DIR }}/bin/mysql -S /tmp/mysql{{ MYSQL_PORT }}.sock -uroot -p8085782 -e "create user tian identified by '8085782'"
+{{ BASE_DIR }}/bin/mysql -S /tmp/mysql{{ MYSQL_PORT }}.sock -uroot -p8085782 -e "grant all on *.* to tian with grant option"
+
